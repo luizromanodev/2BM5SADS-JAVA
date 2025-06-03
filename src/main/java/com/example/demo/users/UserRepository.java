eab0687f479e544dca54fca52540bd56242a00ff
Luiz Romano <160535995+luizromanodev@users.noreply.github.com>
2025-06-02 22:56:03 -0300

    feat: create repository

---
+++ src/main/java/com/example/demo/users/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<UsersModel,Long> {
+    UserDetails findByLogin(String role);
+}
