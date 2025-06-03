e3f78692a16ee5bb7e7e7f2a3bf28870fd3e2e37
Luiz Romano <160535995+luizromanodev@users.noreply.github.com>
2025-06-02 23:02:20 -0300

    feat: create model

---
+++ src/main/java/com/example/demo/users/UserModel.java
@@ -0,0 +1,67 @@
+package com.example.demo.users;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "user")
+@Entity(name = "user")
+public class UserModel implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    private UserRole role;
+
+    public UserModel(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ADMIN")
+                , new SimpleGrantedAuthority("USER"));
+        else return    List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
--- src/main/java/com/example/demo/users/UserRepository.java
+++ src/main/java/com/example/demo/users/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UserRepository extends JpaRepository<UsersModel,Long> {
+public interface UserRepository extends JpaRepository<UserModel,Long> {
     UserDetails findByLogin(String role);
 }
